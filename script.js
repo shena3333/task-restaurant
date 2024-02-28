ca9ffc3a6381470f779c4d20ed84c96b49a4bf69
shena3333 <shena3333@mail.ru>
2024-02-28 13:40:02 +0300

    improve

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const ingredients = [
   { id: 2, name: "Моцарелла", quantity: 5 },
   { id: 3, name: "Базилик", quantity: 2 },
   { id: 4, name: "Чайный пакетик", quantity: 5 },
-  { id: 5, name: "Вода", quantity: 5 }
+  { id: 5, name: "Вода", quantity: 1 }
 ];
 const dishes = [
   { id: 1, name: "Маргарита", ingredients: [1, 2, 3] },
@@ -26,132 +26,44 @@ const orders = [
   { orderId: 2, dishId: 2, status: "новый" },
   { orderId: 3, dishId: 2, status: "готов" }
 ];
+// 1. Проверка возможности приготовления блюда
+function needIngredientsForDish(dishes, ingredients, dishId) {
+  let needIngredient = dishes.find((dish) => dishId === dish.id).ingredients;
+  return ingredients
+    .filter((ingredient) => needIngredient.includes(ingredient.id))
+    .every((realIngredient) => realIngredient.quantity > 0)
+}
 
-// Требования к выполнению:
-// Используйте циклы для обхода массивов.
-// Примените условные конструкции для проверки наличия ингредиентов и изменения статусов заказов.
-// Управляйте объектами для обновления их состояния в соответствии с логикой приложения.
+// 2. Обновление запасов ингредиентов:
+function updateIngredientsAfterDish(dishId, dishes, ingredients) {
+  const dish = dishes.find((dish) => dish.id === dishId);
+  if (needIngredientsForDish(dishes, ingredients, dishId)) {
+    const needIngredient = dishes.find((dish) => dishId === dish.id).ingredients;
+    alert(`Запасы для блюда ${dish.name} уменьшены`)
+    return ingredients
+      .filter((ingredient) => needIngredient.includes(ingredient.id))
+      .forEach((realIngredient) => realIngredient.quantity--)
+  }
+};
 
-// 1. Проверка возможности приготовления блюда: Напишите функцию, которая принимает dishId и проверяет, достаточно ли ингредиентов 
-//на складе для приготовления этого блюда. Функция должна возвращать true, если блюдо можно приготовить, и false - в противном случае.
-function needIngredientsForDish(dishId) {
-  // вытаскиваем массив с ингредиентами, которые нужны для конкретного блюда
-  let needIngredient;
-  for (let i = 0; i < dishes.length; i++) {
-    if (dishes[i].id === dishId) {
-      needIngredient = dishes[i].ingredients
+// 3. Изменение статуса заказа:
+function changeStatusOrder(orderId, newStatus, orders) {
+  orders.find(order => order.orderId === orderId).status = newStatus;
+  alert(`Статус заказа ${orderId} изменен на: ${newStatus}`)
+};
+
+// 4. Автоматическая обработка заказов
+function processOrder(dishes, ingredients, orders) {
+  orders.map((order) => {
+    if (order.status === 'новый') {
+      if (needIngredientsForDish(dishes, ingredients, order.dishId)) {
+        updateIngredientsAfterDish(order.dishId, dishes, ingredients);
+        changeStatusOrder(order.orderId, 'в процессе', orders)
+      } else {
+        changeStatusOrder(order.orderId, 'отменен из-за нехватки ингредиентов', orders)
+      }
+
     }
-  }
-  console.log(needIngredient)
-  // вытаскиваем массив с объектами, где хранятся остатки ингредиентов
-  let needQuantityIngred = [];
-  for (let i = 0; i < ingredients.length; i++) {
-    if (needIngredient.includes(ingredients[i].id)) {
-      needQuantityIngred.push(ingredients[i])
-    }
-  }
-  console.log(needQuantityIngred)
-  // проверка, чтобы нужные ингредиенты были больше 0
-  let needQuantity = needQuantityIngred.every(ing => {
-    return ing.quantity > 0;
   })
-  console.log(needQuantity)
 }
-// needIngredientsForDish(2)
-
-// 2. Обновление запасов ингредиентов: Разработайте функцию, которая обновляет количество ингредиентов на складе после приготовления блюда.
-function updateIngredientsAfterDish(dishId) {
-  // вытаскиваем массив с ингредиентами, которые нужны для конкретного блюда
-  let needIngredient;
-  for (let i = 0; i < dishes.length; i++) {
-    if (dishes[i].id === dishId) {
-      needIngredient = dishes[i].ingredients
-    }
-  }
-  console.log(needIngredient)
-  // вытаскиваем массив с объектами, где хранятся остатки ингредиентов
-  let needQuantityIngred = [];
-  for (let i = 0; i < ingredients.length; i++) {
-    if (needIngredient.includes(ingredients[i].id)) {
-      needQuantityIngred.push(ingredients[i])
-    }
-  }
-  console.log(needQuantityIngred)
-  //уменьшаем запасы на 1 в тех, которые нужны для блюда
-  let newQuanity;
-  newQuanity = needQuantityIngred.map((value) =>
-    value.quantity = value.quantity - 1
-  )
-  return newQuanity
-}
-// updateIngredientsAfterDish(_)
-
-// console.log(ingredients)
-//  3. Изменение статуса заказа: Создайте функцию, которая изменяет статус заказа в массиве orders на основе переданного orderId
-//  и нового статуса. status (статус заказа, например, "новый", "в процессе", "готово", "отменено")
-function changeStatusOrder(orderIdent, newStatus) {
-  let needOrder;
-  for (let i = 0; i < orders.length; i++) {
-    if (orders[i].orderId === orderIdent) {
-      needOrder = orders[i]
-    }
-  }
-  needOrder.status = newStatus;
-  return needOrder
-}
-// console.log(changeStatusOrder(_, 'в процессе'))
-
-// 4. Автоматическая обработка заказов: Напишите функцию, которая автоматически обрабатывает все новые заказы, проверяя
-//возможность их приготовления и, в случае возможности, обновляя запасы и статус заказа.
-function processOrder(mass) {
-  // let newOrders = mass.filter((value) => {
-  //   value.status == 'новый'
-  // });
-  let newOrders = [];
-  newOrders = mass.filter((value) => {
-    return value.status == "новый";
-  });
-  console.log(newOrders);
-  for (let i = 0; i < newOrders.length; i++) {
-    let currentOrder = newOrders[i];
-    // console.log(currentOrder);
-    let currentDish = currentOrder.dishId;
-    // console.log(currentDish);
-    // вытаскиваем массив с ингредиентами, которые нужны для конкретного блюда
-    let needIngredient;
-    for (let i = 0; i < dishes.length; i++) {
-      if (dishes[i].id === currentDish) {
-        needIngredient = dishes[i].ingredients
-      }
-    }
-    // console.log(needIngredient)
-    // вытаскиваем массив с объектами, где хранятся остатки ингредиентов
-    let needQuantityIngred = [];
-    for (let i = 0; i < ingredients.length; i++) {
-      if (needIngredient.includes(ingredients[i].id)) {
-        needQuantityIngred.push(ingredients[i])
-      }
-    }
-    // console.log(needQuantityIngred)
-    // проверка, чтобы нужные ингредиенты были больше 0
-    let needQuantity = needQuantityIngred.every(ing => {
-      return ing.quantity > 0;
-    })
-    // console.log(needQuantity)
-    // переход в статус отменен или в порцессе в зависиммости от остатков
-    if (needQuantity == true) {
-      let newQuanity;
-      newQuanity = needQuantityIngred.map((value) =>
-        value.quantity = value.quantity - 1
-      )
-      currentOrder.status = 'в процессе';
-      alert(`Запас ингредиентов обновлен, заказ ${currentOrder.orderId} в процесс приготовления`);
-    } else {
-      alert(`Не хватает ингредиентов, заказ ${currentOrder.orderId} отменен`);
-      currentOrder.status = 'отменен'
-    }
-  }
-}
-processOrder(orders)
-console.log(orders)
-console.log(ingredients)
+console.log(processOrder(dishes, ingredients, orders), ingredients, orders)
